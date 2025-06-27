f3f80da700e560be70cbbc1b67c4212097f47917
Ryan <ryancatt390@gmail.com>
2025-06-26 23:22:01 -0400

    Add ESLint disable comment

--- components/FoodList.tsx
+++ components/FoodList.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import { REACT_APP_API_KEY } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
